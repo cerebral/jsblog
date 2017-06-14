45314b9d0a32a1681bb94546ff9b15aa51e13a19
Christian Alfoni <christianalfoni@gmail.com>
2017-06-14 19:06:11 +0200

    Logging cookies

--- src/app/executions/common/getSession.js
+++ src/app/executions/common/getSession.js
@@ -3,6 +3,7 @@ import admin from 'firebase-admin';
 function getSession({ props, firebase }) {
   const token = props.req.cookies.__session || null;
 
+  console.log('COOKIES', props.req.cookies);
   if (token) {
     return firebase
       .verifyIdToken(token)
