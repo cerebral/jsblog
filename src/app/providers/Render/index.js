046f0aebb4b9c75b4b2cb80f3d3cf0b67bb52fc7
Christian Alfoni <christianalfoni@gmail.com>
2017-06-15 13:00:00 +0200

    Added manifest and logos

--- src/app/providers/Render/index.js
+++ src/app/providers/Render/index.js
@@ -13,6 +13,8 @@ const render = {
       <html>
         <head>
           <meta name="viewport" content="width=device-width, initial-scale=1">
+          <link rel="manifest" href="/manifest.json">
+          <link rel="icon" href="/logo_48x48.png">
           <style>
             body {
               margin: 0;
